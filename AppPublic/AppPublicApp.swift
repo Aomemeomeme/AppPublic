6a8ad36c617b22f8d662c63271179eca00f1142e
Aomemeomeme <A.omemeomeme@gmail.com>
2023-03-11 23:51:39 +0900

    Initial Commit

---
+++ AppPublic/AppPublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPublicApp.swift
+//  AppPublic
+//
+//  Created by 岡田貴紀 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct AppPublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
